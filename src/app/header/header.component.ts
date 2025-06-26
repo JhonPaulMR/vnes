931103e44b67d16f9b4a5b2f3b066c8d6f9d567f
JhonPaulMR <liptinho2@gmail.com>
2025-06-26 18:15:55 -0300

    removido menu hamburguer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,12 +2,14 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Import CommonModule
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../auth/auth.service'; // Import AuthService
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHouse, faRightToBracket, faGamepad, faGear, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   // Add CommonModule to imports to enable *ngIf
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -17,6 +19,13 @@ export class HeaderComponent {
   
   // Create a direct reference to the currentUser signal from the service
   currentUser = this.authService.currentUser;
+  
+  // Ícones do Header
+  iconHome = faHouse;
+  iconLogin = faRightToBracket;
+  iconCart = faGamepad;
+  iconSettings = faGear;
+  iconLogout = faRightFromBracket;
 
   /**
    * Calls the logout method from the AuthService.
