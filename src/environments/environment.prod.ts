12004f15a1aa2572e09098bd4db66d28569d3284
JhonPaulMR <liptinho2@gmail.com>
2025-06-26 11:18:02 -0300

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000' 
+  apiUrl: 'https://gist.githubusercontent.com/JhonPaulMR/7c60f0877b939e6fd40f55da2e282911/raw/b0d637696160e7d7cf761563c7e967f6543308b2/db.json' 
 };
